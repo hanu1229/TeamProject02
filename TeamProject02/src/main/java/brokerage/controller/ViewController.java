06d515c77577b38bc99711e0bd322092b2b9e9ba
jeenimari <gha0189@gmail.com>
2025-02-12 18:17:20 +0900

    클라이언트 다오 , 클라이언트 서비스 , 페이지 디티오 , 뷰컨트롤러  생성(작성중),서머리 컨트롤러(미생성),인포컨트롤러(미생성)

--- TeamProject02/src/main/java/brokerage/controller/ViewController.java
+++ TeamProject02/src/main/java/brokerage/controller/ViewController.java
@@ -1,5 +1,37 @@
 package brokerage.controller;
 
-public class ViewController {
+import java.io.IOException;
 
-}
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import brokerage.service.ClientService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/client/view")
+public class ViewController extends HttpServlet { //진석작성
+ 
+	private ClientService clientService = ClientService.getInstance();
+	private ObjectMapper mapper = new ObjectMapper();
+	
+	@Override
+		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+			resp.setContentType("application/json");
+		
+			try {
+				
+				
+				
+				
+			}catch (Exception e) {
+				
+			}
+	
+	}
+	
+	
+}// class end
+
