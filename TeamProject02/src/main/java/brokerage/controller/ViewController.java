786ecb347cdc40eb35a4069adaa5dc946ed42371
jeenimari <gha0189@gmail.com>
2025-02-18 18:14:39 +0900

    map jsp , map css , client controller 수정 맵  우측옮김

--- TeamProject02/src/main/java/brokerage/controller/ViewController.java
+++ TeamProject02/src/main/java/brokerage/controller/ViewController.java
@@ -2,11 +2,17 @@ package brokerage.controller;
 
 import java.io.IOException;
 
+
+import java.util.List;
+
+
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import brokerage.model.dto.PageDto;
+import brokerage.model.dao.ClientDao;
+
 import brokerage.model.dto.PropertyDto;
-import brokerage.service.ClientService;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -16,43 +22,29 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/client/view")
 public class ViewController extends HttpServlet { //진석작성
  
-	private ClientService clientService = ClientService.getInstance();
-	private ObjectMapper mapper = new ObjectMapper();
-	
 	@Override
 		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-			resp.setContentType("application/json");
+
+		List<PropertyDto> result = ClientDao.getInstance().findAllProperties();
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonResult = mapper.writeValueAsString(result);
+		resp.setContentType("application/json");
+		resp.getWriter().print(jsonResult);
 		
-			try {
-				
-			String pnoStr = req.getParameter("pno");
-			
-			//pno 파라미터가 없으면 전체 목록 조회
-			if(pnoStr ==null) {
-				String pageStr = req.getParameter("page");
-				int page =(pageStr!=null)?Integer.parseInt(pageStr):1;
-				
-				PageDto pageDto = clientService.getPropertyList(page);
-				String jsonResult = mapper.writeValueAsString(pageDto);
-				resp.getWriter().print(jsonResult);
-			}
-				//pno(매물번호) 가 있으면 상세조회
-			else {
-				int pno = Integer.parseInt(pnoStr);
-				PropertyDto property = clientService.getPropertyDetail(pno);
-				String jsonResult = mapper.writeValueAsString(property);
-				resp.getWriter().print(jsonResult);
-				
-					
-			}
-			}catch (Exception e) {
-				e.printStackTrace();
-				resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-				resp.getWriter().print(e.getMessage());
-			}
-	
+		
 	}
 	
 	
 }// class end
 
+//기존 전체목록조회(페이징)
+//
+////pno 파라미터가 없으면 전체 목록 조회
+//if(pnoStr ==null) {
+//	String pageStr = req.getParameter("page");
+//	int page =(pageStr!=null)?Integer.parseInt(pageStr):1;
+//	
+//	PageDto pageDto = clientService.getPropertyList(page);
+//	String jsonResult = mapper.writeValueAsString(pageDto);
+//	resp.getWriter().print(jsonResult);
+//}
