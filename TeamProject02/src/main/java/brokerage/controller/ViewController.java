e5dbf23d957de7227830c4dbf59a20a0a87654a5
hanu1229 <han494127@gmail.com>
2025-02-19 18:05:01 +0900

    오류 수정3

--- TeamProject02/src/main/java/brokerage/controller/ViewController.java
+++ TeamProject02/src/main/java/brokerage/controller/ViewController.java
@@ -43,6 +43,7 @@ public class ViewController extends HttpServlet { //진석작성
 			for(int i =0; i<=imges.size()-1;i++) {
 				//String str = req.getServletContext().getRealPath("/img/")+imges.get(i);
 				// 이부분이 수정
+				// 이부분이 수정이에요
 				String str = "/TeamProject02/img/" + imges.get(i);
 				result.get(i).setPhoto(str);
 				//System.out.println(result.get(i).getPhoto());
