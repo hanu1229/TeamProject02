64441e02fb711f2cc97fe5a8d38083770acf66d1
leeminjin98 <lmj98202@gmail.com>
2025-02-19 14:47:02 +0900

    회원가입 js , jsp 완

--- TeamProject02/src/main/webapp/js/member/member-signup.js
+++ TeamProject02/src/main/webapp/js/member/member-signup.js
@@ -3,23 +3,27 @@ console.log('member-signup.js!!');
 // 회원가입 js
 const memberSignUp = ( ) => {
 	
-	//	1. form 을 한번에 가져오기
-	const msignupform = document.querySelector('#signupform'); // form 전체 가져오기
-	// console.log( msiupform );
+	//	입력한 값 가져오기
+	const mid = document.querySelector('.midinput').value;
+	const mpwd = document.querySelector('.mpwdinput').value;
+	const mname = document.querySelector('.mnameinput').value;
+	const mphone = document.querySelector('.mphoneinput').value;
 	
-	// FormData
-	const msignupformData = new FormData( msignupform );
+	// 객체화
+	const obj = { mid : mid , mpwd : mpwd , mname : mname , mphone : mphone }
+	 
 	// option
 	const option = {
 		method : 'POST' ,
-		body : msignupformData
+		headers : { 'Content-Type' : 'application/json' },
+		body : JSON.stringify( obj )
 	}
 	
 	// fetch
 	fetch(`/TeamProject02/afiliado/signup` , option )
 		.then( response => response.json() )
 		.then( data => {
-			if( data == true ){ alert('회원가입 성공'); location.reload(); }
+			if( data == true ){ alert('회원가입 성공'); location.href = "memberLogin.jsp"; }
 			else{ alert('회원가입 실패'); }
 		}) // thed end
 		.catch( error => { console.log( error ); } )
